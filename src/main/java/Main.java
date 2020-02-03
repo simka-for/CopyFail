8f048d8ede57d1b352470a504e9630cc10014c95
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-02-03 13:53:18 +0300

    add new metod

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,37 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Main {
     public static void main(String[] args) {
+
         File input = new File("D:/spisok");
         File output = new File("D:/test");
+
         try {
             FileUtils.copyDirectory(input, output);
         } catch (IOException e) {
             e.printStackTrace();
         }
+//        try {
+//            copyDirect(input, output);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private static void copyDirect(File input, File output) throws IOException {
+//        File folder = new File(String.valueOf(input));
+//        File[] listOfFiles = folder.listFiles();
+//        Path destDir = Paths.get(String.valueOf(output));
+//
+//        if (listOfFiles != null)
+//            for (File file : listOfFiles)
+//                Files.copy(file.toPath(), destDir.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
+//    }
     }
 }
